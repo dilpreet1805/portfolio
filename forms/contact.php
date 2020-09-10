bee3c1c02ac96f8c13a5b94e71e00d5ed4406f82
admin <dilpreet1851990@gmail.com>
2020-09-10 11:10:51 +0530

    portfolio

---
+++ forms/contact.php
@@ -0,0 +1,13 @@
+<?php
+    if($_SERVER['REQUEST_METHOD']=="POST")
+	{
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+        $description = $_POST['desc'];
+    }
+    $to = "dilpreet1851990@gmail.com";
+	$subject = "Received Mail from Portfolio";
+	echo $txt = "HELLO"  ."\r\n". "Someone tried reaching your details are as follows"."\r\n".$email."\r\n".$description."";
+    $headers = "From:".$email."";
+    mail($to,$subject,$txt,$headers);
+?>
